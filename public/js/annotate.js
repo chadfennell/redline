d03b027122fade17ef80d5ac882162e061258fe3
Chad Fennell <libsys@gmail.com>
2013-06-01 16:10:32 -0500

    make checkboxes behave like radio buttons

--- public/js/annotate.js
+++ public/js/annotate.js
@@ -1,2 +1,14 @@
 $('.xtend').annotator()
   .annotator('addPlugin', 'Vote');
+
+
+$( document ).ready(function() {
+  // Jank hack to make checkboxes behave like radios b/c radios
+  // are not yet implemented in annotator
+  $('#annotator-field-2').click(function() {
+    $('#annotator-field-1').attr('checked', false);
+  });
+  $('#annotator-field-1').click(function() {
+    $('#annotator-field-2').attr('checked', false);
+  });
+});
